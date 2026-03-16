23370c4c8f6c2ae057301d05db7ecc28832018eb
TheK4mm <juankvas20056@gmail.com>
2026-03-15 23:52:35 -0500

    fix database conection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "biblioteca_personal",
-  password: "kampost09",
-  port: 5432
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    })
+  : new Pool({
+      user: "postgres",
+      host: "localhost",
+      database: "biblioteca_personal",
+      password: "kampost09",
+      port: 5432
+    });
 
 pool.connect()
   .then(() => {
